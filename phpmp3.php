8f7b6ace77dd05bce13d14a31bacfc466183d71d
David Gallagher <david@thegallagher.net>
2016-02-15 15:12:33 +1100

    Clean up code, add phpdoc

--- phpmp3.php
+++ phpmp3.php
@@ -1,24 +1,45 @@
 <?php
-/*
- * PHPMP3
- * 
- * Version 0.9.0
- * 
- * Modified version mp3 class from http://www.sourcerally.net/Scripts/20-PHP-MP3-Class
- * This version was at http://www.ClickFM.co.il/temp/ofer/mp3.txt (dead link, retrieved from http://web.archive.org/web/http://www.ClickFM.co.il/temp/ofer/mp3.txt)
- * Suggested change from kamilmajewski (26-12-2008 12:45) in comments from http://www.sourcerally.net/Scripts/20-PHP-MP3-Class.
- * Licence: LGPL
+/**
+ * Class PHPMP3
+ *
+ * @license LGPL
  */
-
-// TODO: Use fopen and fseek instead of reading in the whole file all at once.
 class PHPMP3
 {
 
+    /**
+     * The full mp3 file
+     *
+     * @var string
+     */
     private $str;
+
+    /**
+     * The time length of the current file
+     *
+     * @var
+     */
     private $time;
+
+    /**
+     * The amount of frames in the current file
+     *
+     * @var
+     */
     private $frames;
+
+    /**
+     * Translate ascii characters to binary
+     *
+     * @var array
+     */
     private $binaryTable;
 
+    /**
+     * Construct a new instance
+     *
+     * @param string $path Path to an mp3 file
+     */
     public function __construct($path = '')
     {
         $this->binaryTable = array();
@@ -31,11 +52,22 @@ class PHPMP3
         }
     }
 
+    /**
+     * Set the mp3 data
+     *
+     * @param string $str Mp3 file
+     * @return void
+     */
     private function setStr($str)
     {
         $this->str = $str;
     }
 
+    /**
+     * Get the start of audio data
+     *
+     * @return bool|int|void
+     */
     public function getStart()
     {
         $currentStrPos = - 1;
@@ -60,6 +92,11 @@ class PHPMP3
         }
     }
 
+    /**
+     * Manually count the frames and time of the file
+     *
+     * @return bool
+     */
     public function setFileInfoExact()
     {
         $maxStrLen     = strlen($this->str);
@@ -94,6 +131,13 @@ class PHPMP3
         return true;
     }
 
+    /**
+     * Extract a portion of an mp3
+     *
+     * @param int $start Time in seconds to extract from
+     * @param int $length Time in seconds to extract
+     * @return static
+     */
     public function extract($start, $length)
     {
         $maxStrLen     = strlen($this->str);
@@ -134,6 +178,12 @@ class PHPMP3
         return $mp3;
     }
 
+    /**
+     * Get the length of a frame in bytes and seconds
+     *
+     * @param string[] $parts A frame with bytes converted to binary
+     * @return array|bool
+     */
     private function doFrameStuff($parts)
     {
         //Get Audio Version
@@ -189,7 +239,7 @@ class PHPMP3
             '1111' => array(- 1, - 1, - 1, - 1, - 1)
         );
         $bitPart  = $bitArray[$bitFlag];
-        $bitArrayNumber; //  TODO: Remove this line or set a value
+        $bitArrayNumber = null;
         if ($audio == 1) {
             switch ($layer) {
                 case 1:
@@ -220,7 +270,6 @@ class PHPMP3
             return false;
         }
         //Get Frequency
-        //Change from kamilmajewski (26-12-2008 12:45) in comments from http://www.sourcerally.net/Scripts/20-PHP-MP3-Class
         $frequencies = array(
             1   => array(
                 '00' => 44100,
@@ -254,11 +303,25 @@ class PHPMP3
         }
         $seconds += $frameLength * 8 / ($bitRate * 1000);
         return array($frameLength, $seconds);
-        //Calculate next when next frame starts.
-        //Capture next frame.
     }
 
-    // TODO: Could be replaced by PHP ID3 (http://php.net/manual/en/book.id3.php) or another library
+    /**
+     * Set ID3 data
+     *
+     * @param string $track
+     * @param string $title
+     * @param string $artist
+     * @param string $album
+     * @param string $year
+     * @param string $genre
+     * @param string $comments
+     * @param string $composer
+     * @param string $origArtist
+     * @param string $copyright
+     * @param string $url
+     * @param string $encodedBy
+     * @return void
+     */
     public function setIdv3_2(
         $track,
         $title,
@@ -273,7 +336,6 @@ class PHPMP3
         $url,
         $encodedBy
     ) {
-        // TODO: If I use this function remove the casting and set defaults values for all strings.
         $urlLength        = (int) (strlen($url) + 2);
         $copyrightLength  = (int) (strlen($copyright) + 1);
         $origArtistLength = (int) (strlen($origArtist) + 1);
@@ -287,21 +349,37 @@ class PHPMP3
         $trackLength      = (int) (strlen($track) + 1);
         $yearLength       = (int) (strlen($year) + 1);
 
-        // TODO: There must be a better way to do this.
-        $str .= "ID3\x03\0\0\0\0\x085TRCK\0\0\0{$trackLength}\0\0\0{$track}TENC\0\0\0{$encodedByLength}@\0\0{$encodedBy}WXXX\0\0\0{$urlLength}\0\0\0\0{$url}TCOP\0\0\0{$copyrightLength}\0\0\0{$copyright}TOPE\0\0\0{$origArtistLength}\0\0\0{$origArtist}TCOM\0\0\0{$composerLength}\0\0\0{$composer}COMM\0\0\0{$commentsLength}\0\0\0\0\x09\0\0{$comments}TCON\0\0\0{$genreLength}\0\0\0{$genre}TYER\0\0\0{$yearLength}\0\0\0{$year}TALB\0\0\0{$albumLength}\0\0\0{$album}TPE1\0\0\0{$artistLength}\0\0\0{$artist}TIT2\0\0\0{$titleLength}\0\0\0{$title}";
+        $str = "ID3\x03\0\0\0\0\x085TRCK\0\0\0{$trackLength}\0\0\0{$track}TENC\0\0\0{$encodedByLength}@\0\0{$encodedBy}WXXX\0\0\0{$urlLength}\0\0\0\0{$url}TCOP\0\0\0{$copyrightLength}\0\0\0{$copyright}TOPE\0\0\0{$origArtistLength}\0\0\0{$origArtist}TCOM\0\0\0{$composerLength}\0\0\0{$composer}COMM\0\0\0{$commentsLength}\0\0\0\0\x09\0\0{$comments}TCON\0\0\0{$genreLength}\0\0\0{$genre}TYER\0\0\0{$yearLength}\0\0\0{$year}TALB\0\0\0{$albumLength}\0\0\0{$album}TPE1\0\0\0{$artistLength}\0\0\0{$artist}TIT2\0\0\0{$titleLength}\0\0\0{$title}";
         $this->str = $str . $this->str;
     }
 
+    /**
+     * Append another mp3 file to the end of this file
+     *
+     * @param self $mp3
+     * @return void
+     */
     public function mergeBehind(self $mp3)
     {
         $this->str .= $mp3->str;
     }
 
+    /**
+     * Prepend another mp3 to the start of this file
+     *
+     * @param self $mp3
+     * @return void
+     */
     public function mergeInfront(self $mp3)
     {
         $this->str = $mp3->str . $this->str;
     }
 
+    /**
+     * Get the end string of the ID3 data
+     *
+     * @return bool|string
+     */
     private function getIdvEnd()
     {
         $strlen = strlen($this->str);
@@ -314,11 +392,15 @@ class PHPMP3
         }
     }
 
+    /**
+     * Remove ID3 data from the file
+     *
+     * @return bool
+     */
     public function striptags()
     {
         //Remove start stuff...
-        $newStr = '';
-        $s      = $start = $this->getStart();
+        $s = $start = $this->getStart();
         if ($s === false) {
             return false;
         } else {
@@ -331,6 +413,12 @@ class PHPMP3
         }
     }
 
+    /**
+     * Write an mp3 file
+     *
+     * @param string $path Path to write file to
+     * @return bool
+     */
     public function save($path)
     {
         $fp           = fopen($path, 'w');
@@ -339,7 +427,13 @@ class PHPMP3
         return $bytesWritten == strlen($this->str);
     }
 
-    //join various MP3s
+    /**
+     * Join multiple mp3 files into one file
+     *
+     * @param string $newpath
+     * @param array $array
+     * @return void
+     */
     public function multiJoin($newpath, $array)
     {
         foreach ($array as $path) {
